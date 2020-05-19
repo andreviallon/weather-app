564926efdb6dfa3ce231d85ee8e6f11565f8a3f2
andre_viallon <viallon.andre@gmail.com>
2020-05-19 16:14:02 +0200

    Add react-moment

--- src/components/ForecastCard/ForecastCard.jsx
+++ src/components/ForecastCard/ForecastCard.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 import './ForecastCard.scss';
 
 export const ForecastCard = ({ forecast }) => {
 
-    // const imgUrl = `http://openweathermap.org/img/wn/${forecast.icon}@2x.png`;
+    const imgUrl = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
 
-    const dayOfTheWeek = Date.now();
+    // const date = moment(forecast.dt);
 
-    console.log('forecast day', forecast.dt, dayOfTheWeek);
+    // console.log('date', date);
 
     return (
         <div className="card">
             <div className="card-body">
-            <h5 className="card-title">{forecast.day}</h5>
-                {/* <img className="forecast-img" src={imgUrl} alt="forecast icon" />
+                <h5 className="card-title"><Moment unix format="DD MMM">{forecast.dt}</Moment></h5>
+                <img className="forecast-img" src={imgUrl} alt="forecast icon" />
                 <div className="temperature-container">
-                    <span className="max-temp card-text">{forecast.maxTemp}&deg;</span>
-                    <span className="min-temp card-text">{forecast.minTemp}&deg;</span>
-                </div> */}
+                    <span className="max-temp card-text">{forecast.temp.max}&deg;</span>
+                    <span className="min-temp card-text">{forecast.temp.min}&deg;</span>
+                </div>
             </div>
         </div>
     );
